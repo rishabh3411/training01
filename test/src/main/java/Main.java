2d72bf3265c0305cb9cf2c9f83b5c86a8eafb7e6
Rishabh <pathakrishabh3411@gmail.com>
2024-04-08 22:19:04 +0530

    encapsulation 1.1

--- test/src/main/java/Main.java
+++ test/src/main/java/Main.java
@@ -2,11 +2,15 @@ import org.studyeasy.person;
 
 public class Main {
     public static void main(String[]args){
-        person person = new person("Rishabh",50,"male");
-        System.out.println(person);
-        person.setName("Rishu");
-        System.out.println(person);
-        person.setAge(149.5);
-        System.out.println(person);
+        person calling =new person("rishu",19,"male");
+        System.out.println(calling);
+        calling.setName("mickey");
+        System.out.println(calling);
+        calling.setAge(111);
+        System.out.println(calling);
+        calling.setName("mickey");
+        System.out.println(calling);
+       calling.setGender("female");
+        System.out.println(calling);
     }
 }
--- test/src/main/java/org/studyeasy/person.java
+++ test/src/main/java/org/studyeasy/person.java
@@ -2,27 +2,31 @@ package org.studyeasy;
 
 public class person {
     private String name;
-    private double age;
+    private int age;
     private String gender;
 
     public person(String name, int age, String gender) {
         this.name = name;
         this.age = age;
-        this.gender = gender;
+       this.gender = gender;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public boolean setAge(double age) {
+    public boolean setAge(int age) {
         if (age >= 0 && age <= 150) {
-            this.age = age;
+            this.age=age;
             return true;
-        } else {
+        }else {
             return false;
         }
 
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
     }
 
     @Override
