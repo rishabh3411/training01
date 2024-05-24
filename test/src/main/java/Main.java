b499f0641746a41c0788c90bc0cb30fb5dc72ef2
Rishabh <pathakrishabh3411@gmail.com>
2024-05-25 01:45:03 +0530

    CURRENCY CONVERTER PROJECT

--- test/src/main/java/Main.java
+++ test/src/main/java/Main.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Currency;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+        CurrencyConverter currencyConverter = new CurrencyConverter();
+
+        System.out.println("Welcome to Currency Converter !");
+        System.out.println("Enter amount to convert:");
+        double amount = scanner.nextDouble();
+
+        System.out.println("Select Currency to convert from (USD, EUR, GBR, JPY):");
+        String fromCurrency = scanner.next().toUpperCase();
+
+        System.out.println("select currency yo convert to (USD, EUR, GBR, JPY);");
+        String toCurrency = scanner.next().toUpperCase();
+
+        double convertedAmount = currencyConverter.convert(amount,fromCurrency,toCurrency);
+        System.out.printf("%.2f %s = %.2f %s\n", amount, fromCurrency, convertedAmount, toCurrency);
+    }
 }
