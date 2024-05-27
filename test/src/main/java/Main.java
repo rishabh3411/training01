7a93e6c7c4012b2632d95d65267d67d07f46b9cb
Rishabh <pathakrishabh3411@gmail.com>
2024-05-27 23:25:37 +0530

    java static elements

--- test/src/main/java/Main.java
+++ test/src/main/java/Main.java
@@ -1,22 +1,15 @@
-import java.util.Currency;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[]args){
-        Scanner scanner = new Scanner(System.in);
-        CurrencyConverter currencyConverter = new CurrencyConverter();
-
-        System.out.println("Welcome to Currency Converter !");
-        System.out.println("Enter amount to convert:");
-        double amount = scanner.nextDouble();
-
-        System.out.println("Select Currency to convert from (USD, EUR, GBR, JPY):");
-        String fromCurrency = scanner.next().toUpperCase();
-
-        System.out.println("select currency yo convert to (USD, EUR, GBR, JPY);");
-        String toCurrency = scanner.next().toUpperCase();
-
-        double convertedAmount = currencyConverter.convert(amount,fromCurrency,toCurrency);
-        System.out.printf("%.2f %s = %.2f %s\n", amount, fromCurrency, convertedAmount, toCurrency);
+// it is a code that shows the static elements
+        //that how the static elements works and how they were implemented.
+        System.out.println(" staticvar = "+ teststatic.getStaticvar());
+        teststatic.setStaticvar(11);
+        System.out.println(" staticvar = "+teststatic.getStaticvar());
+        System.out.println(" staticvar = "+ teststatic.getStaticvar());
+        teststatic.setStaticvar(45);
+        System.out.println(" staticvar = "+teststatic.getStaticvar());
+        System.out.println(" staticvar = "+teststatic.getStaticvar());
+        teststatic.setStaticvar(69);
+        System.out.println(" staticvar = "+ teststatic.getStaticvar());
     }
 }
---
+++ test/src/main/java/teststatic.java
@@ -0,0 +1,11 @@
+public class teststatic {
+    public static int staticvar = 0;
+
+    public static int getStaticvar() {
+        return staticvar;
+    }
+
+    public static void setStaticvar(int staticvar) {
+        teststatic.staticvar = staticvar;
+    }
+}
