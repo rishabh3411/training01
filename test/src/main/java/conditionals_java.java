f630b583b9777ad12ddc1d33215342b307df828d
Rishabh <pathakrishabh3411@gmail.com>
2024-06-17 19:44:52 +0530

    condiitionals_java

---
+++ test/src/main/java/conditionals_java.java
@@ -0,0 +1,85 @@
+import java.util.Scanner;
+
+public class conditionals_java {
+    public static void main(String[] args) {
+    /*
+     //if else statements
+        int age = 18;
+        if (age >= 18) {
+            System.out.println("Eligible for driving");
+        } else {
+            System.out.println("firstly grow up champ");
+        }
+
+        // USE OF LOGICAL OPERATIONS
+        System.out.println("For Logical AND (&&)");
+     Boolean a = true;
+     Boolean b = true;
+     Boolean c = true;
+        if (a && b && c){
+         System.out.println("y");
+     }
+     else {
+         System.out.println("N");
+     }
+
+
+        System.out.println("For Logical OR (||)");
+        Boolean x = true;
+        Boolean y = false;
+        Boolean z = false;
+        if (x || y || z){
+            System.out.println("y");
+        }
+        else {
+            System.out.println("N");
+        }
+        System.out.println("For NOT operator(!) ,inverter");
+        Boolean p = true;
+        Boolean q = false;
+        System.out.println(!p );
+
+
+
+        // ELSE IF CLAUSE (LADDER):
+        System.out.println("Enter the age :");
+        Scanner sc = new Scanner(System.in);
+
+        int age;
+        age = sc.nextInt();
+        if (age>56){
+            System.out.println("Person is experienced");
+        } else if (age>46) {
+            System.out.println("Person is semi-experienced");
+        } else if (age>36) {
+            System.out.println("Person is semi semi-experienced");
+        }else{
+            System.out.println("Person is not experienced");
+        }
+
+
+
+        //SWITCH CASE :
+
+        System.out.println("Enter the age :");
+        Scanner sc = new Scanner(System.in);
+
+        String letter = "rishabh";
+       // age = sc.nextInt();
+       switch (letter){
+           case "tommy":
+               System.out.println("You are going to be an adult!");
+               break;
+           case "romy":
+               System.out.println("You are eligible for job!");
+               break;
+           case "xiaomi":
+               System.out.println("you are going to retire!");
+               break;
+           default:
+               System.out.println("Enjoy your life");
+               break;
+       }
+       */
+    }
+}
